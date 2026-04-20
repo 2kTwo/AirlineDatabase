31a4af582ea5a71a17cccc90d8764e4f56f95409
elie <elyasor888@gmail.com>
2026-04-20 18:55:14 -0400

    CLI working with modified data

--- CLI.py
+++ CLI.py
@@ -1,5 +1,5 @@
 import csv
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 
 class OpenFlightsCLI:
@@ -8,30 +8,57 @@ class OpenFlightsCLI:
         self.airports = []
         self.airlines = []
         self.routes = []
+        self.cities = []
 
+        self.city_by_id = {}
         self.airport_by_code = {}
         self.airports_by_country = defaultdict(list)
         self.routes_by_source = defaultdict(list)
         self.routes_by_destination = defaultdict(list)
         self.airlines_by_code = {}
 
+    def load_cities(self):
+        with open("cities.dat", encoding="utf-8") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                city = {
+                    "city_id": row[0],
+                    "city": row[1],
+                    "country": row[2],
+                    "timezone": row[3],
+                    "dst": row[4],
+                    "tz": row[5]
+                }
+
+                self.cities.append(city)
+                self.city_by_id[city["city_id"]] = city
+
     def load_airports(self):
         with open("airports.dat", encoding="utf-8") as f:
             reader = csv.reader(f)
             for row in reader:
+                city_info = self.city_by_id.get(row[2], {})
+
                 airport = {
                     "id": row[0],
                     "name": row[1],
-                    "city": row[2],
-                    "country": row[3],
-                    "iata": row[4],
-                    "icao": row[5]
+                    "city_id": row[2],
+                    "city": city_info.get("city", "Unknown"),
+                    "country": city_info.get("country", "Unknown"),
+                    "timezone": city_info.get("timezone", ""),
+                    "dst": city_info.get("dst", ""),
+                    "tz": city_info.get("tz", ""),
+                    "iata": row[3],
+                    "icao": row[4]
                 }
 
                 self.airports.append(airport)
 
                 if airport["iata"] != "\\N":
                     self.airport_by_code[airport["iata"]] = airport
+
+                if airport["icao"] != "\\N":
+                    self.airport_by_code[airport["icao"]] = airport
 
                 self.airports_by_country[airport["country"]].append(airport)
 
@@ -53,6 +80,9 @@ class OpenFlightsCLI:
                 if airline["iata"] != "\\N":
                     self.airlines_by_code[airline["iata"]] = airline
 
+                if airline["icao"] != "\\N":
+                    self.airlines_by_code[airline["icao"]] = airline
+
     def load_routes(self):
         with open("routes.dat", encoding="utf-8") as f:
             reader = csv.reader(f)
@@ -62,6 +92,7 @@ class OpenFlightsCLI:
                     "source": row[2],
                     "destination": row[4],
                     "stops": row[7],
+                    "equipment": row[8],
                     "departure": row[-2],
                     "arrival": row[-1]
                 }
@@ -73,10 +104,12 @@ class OpenFlightsCLI:
     def load_all(self):
         print("Loading data...")
 
+        self.load_cities()
         self.load_airports()
         self.load_airlines()
         self.load_routes()
 
+        print(f"{len(self.cities)} cities loaded")
         print(f"{len(self.airports)} airports loaded")
         print(f"{len(self.airlines)} airlines loaded")
         print(f"{len(self.routes)} routes loaded")
@@ -195,11 +228,11 @@ class OpenFlightsCLI:
             print("No routes found between airports")
             return
         
-        print(f"\nFlights between {source} and {dest}: \n")
+        print(f"\nFlights between {source} and {dest}:\n")
 
         for route in routes:
             print(route)
-            print("\n")
+            print()
 
     def get_airport_name(self, code):
         airport = self.airport_by_code.get(code)
@@ -214,47 +247,48 @@ class OpenFlightsCLI:
         return code
 
     def print_board(self, title, rows):
-        print("\n" + "=" * 85)
-        print(title.center(85))
-        print("=" * 85)
-        print(f'{"Airline":25} {"From/To":30} {"Departure":12} {"Arrival":12}')
-        print("-" * 85)
+        print("\n" + "=" * 110)
+        print(title.center(110))
+        print("=" * 110)
+        print(f'{"Airline":25} {"From":25} {"To":25} {"Departure":12} {"Arrival":12}')
+        print("-" * 110)
 
         for row in rows:
             print(
                 f'{row["airline"][:25]:25} '
-                f'{row["location"][:30]:30} '
+                f'{row["from"][:25]:25} '
+                f'{row["to"][:25]:25} '
                 f'{row["departure"]:12} '
                 f'{row["arrival"]:12}'
             )
 
-        print("=" * 85)
+        print("=" * 110)
 
     def board_maker(self):
-        code = input("Enter airport code: \n").upper().strip()
+        code = input("Enter airport code:\n").upper().strip()
 
         departures = []
         arrivals = []
 
-        for route in self.routes_by_source.get(code, []):
+        airport_name = self.get_airport_name(code)
 
+        for route in self.routes_by_source.get(code, []):
             departures.append({
                 "airline": self.get_airline_name(route["airline"]),
-                "location": self.get_airport_name(route["destination"]),
+                "from": airport_name,
+                "to": self.get_airport_name(route["destination"]),
                 "departure": route["departure"],
                 "arrival": route["arrival"]
             })
 
         for route in self.routes_by_destination.get(code, []):
-
             arrivals.append({
                 "airline": self.get_airline_name(route["airline"]),
-                "location": self.get_airport_name(route["source"]),
+                "from": self.get_airport_name(route["source"]),
+                "to": airport_name,
                 "departure": route["departure"],
                 "arrival": route["arrival"]
             })
-
-        airport_name = self.get_airport_name(code)
 
         print(f"\nAirport board for {airport_name}")
 
@@ -268,6 +302,54 @@ class OpenFlightsCLI:
         else:
             print("\nNo arrivals found.")
 
+    def aircraft_count(self):
+        iso = input("Enter country ISO code: ").upper().strip()
+
+        country_name = None
+
+        with open("countries.dat", encoding="utf-8") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[1].upper() == iso:
+                    country_name = row[0]
+                    break
+
+        if not country_name:
+            print("Country ISO code not found.")
+            return
+
+        airports = self.airports_by_country.get(country_name)
+
+        if not airports:
+            print("No airports found for this country.")
+            return
+
+        airport_codes = {
+            airport["iata"] for airport in airports
+            if airport["iata"] != "\\N"
+        }
+
+        aircraft_counter = Counter()
+
+        for route in self.routes:
+            if route["source"] in airport_codes:
+                equipment = route.get("equipment")
+
+                if equipment and equipment != "\\N":
+                    planes = equipment.split()
+
+                    for plane in planes:
+                        aircraft_counter[plane] += 1
+
+        if not aircraft_counter:
+            print("No aircraft data found.")
+            return
+
+        print(f"\nTop 3 aircraft used for flights from {country_name} ({iso}):\n")
+
+        for aircraft, count in aircraft_counter.most_common(3):
+            print(f"{aircraft} : {count} flights")
+
     def menu(self):
 
         while True:
@@ -280,8 +362,9 @@ class OpenFlightsCLI:
             print("5 Airports in a country")
             print("6 Airline route count")
             print("7 Flight finder")
-            print("8 Arrivals/Departues board")
-            print("9 Exit")
+            print("8 Arrivals/Departures board")
+            print("9 Aircraft by country")
+            print("0 Exit")
 
             choice = input("Choose option: ")
 
@@ -310,6 +393,9 @@ class OpenFlightsCLI:
                 self.board_maker()
 
             elif choice == "9":
+                self.aircraft_count()
+
+            elif choice == "0":
                 break
 
             else:
